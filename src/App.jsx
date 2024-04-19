d2a1e1a5c7e56dd81e0fabdaf99f34f313754e7d
Rodrigo <azeythy1@gmail.com>
2024-04-18 22:07:36 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import { Dados } from './dados'
+
+function App() {
+  
+
+  return (
+    <>
+   <Dados 
+    nome=" Rodrigo"
+    cpf="015.852.745-49"
+    cel="35 992436519"
+    end="RUA MARIA CONCEICÃO N65"
+    cep= "37152-000"
+    cidade="Varginha"
+    modelo="Fone jbl"
+    cor="preto"
+    f_pag="Pix"
+   />
+  <Dados 
+    nome=" Flavio"
+    cpf="015.852.745-49"
+    cel="35 991355257"
+    end="Av Brasil N605"
+    cep= "37152-222"
+    cidade="Varginha"
+    modelo="Capinha"
+    cor="Rosa"
+    f_pag="Credito 12x"
+   />
+    </>
+  )
+}
+
+export default App
