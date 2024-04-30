ef5d4c4767834027448b9201802be387f9870180
Azeythy1 <azeythy1@gmail.com>
2024-04-30 12:53:04 -0300

    dados

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import'./App.css'
+import { useState } from 'react'
 import { Header } from './components/Header'
 import { Menu } from './components/Menu'
-import { Card } from './components/Card'
+// import { Card } from './components/Card'
 function App() {
+  const [dados, setDados]= useState([
+
+    {
+              nome:"Doce",
+              modelo:"Xiami 12",
+              tipo:"assistencia",
+              status:"completo",
+              preco:"150",
+              id:1,
+    },
+    {
+              nome:"Doce",
+              modelo:"Xiami 12",
+              tipo:"assistencia",
+              status:"completo",
+              preco:"150",
+              id:2
   
+    },
+  ]);
 
   return (
-    <>
+  <div>
     <Header/>
     <Menu/>
     <div className="body">
@@ -14,38 +34,18 @@ function App() {
       <button>Pesquisar</button>
       <input type="search" />
       
-      <Card 
-        nome="Flavia"
-        modelo="Note 13 256+8"
-        tipo="Assitencia"
-        status="Analise"
-        preco="150"
-        />
-      <Card 
-       nome="Flavia"
-       modelo="Note 13 256+8"
-       tipo="Assitencia"
-       status="Analise"
-       preco="150"/>
-      <Card
-       nome="Flavia"
-       modelo="Note 13 256+8"
-       tipo="Assitencia"
-       status="Analise"
-       preco="150"/>
-      <Card
-       nome="Flavia"
-       modelo="Note 13 256+8"
-       tipo="Assitencia"
-       status="Analise"
-       preco="150"/>
+      {dados.map((dados)=>(
+        
+          <div>
+            <p>{dados.nome}</p>
+            <p>{dados.id}</p>
+          </div>  
+          
+      ))}
     </div>
-
-
-
-
-    </>
-  )
+  
+  </div>
+    )
 }
 
 export default App
