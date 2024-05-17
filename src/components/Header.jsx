487cb175b5b56e3dfb64fa8f05265661e7002967
Azeythy1 <azeythy1@gmail.com>
2024-05-17 13:35:38 -0300

    nada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import styles from './Header.module.css'
+import logo from './img/logo.png'
 
 export function Header(){
   
@@ -11,10 +12,11 @@ return(
         <h1 className={styles.header}>
             Eletroinfo Outlet
         </h1>
-        <img className={styles.logo} src="../src/img/logo.png" alt="logo" />
+        <img className={styles.logo} src={logo} alt="logo" />
     </div>
     </>
 
 )
 
 }
+
